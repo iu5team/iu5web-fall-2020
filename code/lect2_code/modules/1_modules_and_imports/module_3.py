da1e830d320e5430952d600b1c58ffebd145edc9
ygapanyuk <Gapanyuk.Yury@yandex.ru>
2020-09-14 08:57:18 +0300

    Лекция и лаба 2

---
+++ code/lect2_code/modules/1_modules_and_imports/module_3.py
@@ -0,0 +1,26 @@
+import sys
+
+print("Importing module 3")
+
+def sum1(a, b):
+    print("Running sum1 from module 3")
+    return a + b
+
+if __name__ == "__main__":
+    print("This code is not running when importing module. " + \
+        "Only when executing as script.")
+    # Command line params
+    if len(sys.argv) > 1:
+        print("Param 0 = {} \n\n".format(sys.argv[0]))
+        for i, p in enumerate(sys.argv):
+            print("Param {} = {}".format(i, p))
+
+        try:
+            a = float(sys.argv[1])
+            b = float(sys.argv[2])
+            print('{} + {} = {}'.format(a, b, sum1(a,b)))
+        except:
+            pass
+
+    else:
+        print("No command line params")
